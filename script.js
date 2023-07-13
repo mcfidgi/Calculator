e905bf3be22e93357e8ddd9a4f1a1134f7216b42
Max <53892086+mcfidgi@users.noreply.github.com>
2023-07-13 18:13:28 -0400

    Add eventListener to the clear button and make the display actually display things

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function operate(firstNumber, operator, secondNumber) {
     } else {alert(`Something went wrong. Operator error`)}
 };
 
-
+const display = document.querySelector("#display");
 
 
 const one = document.querySelector(`.one`);
@@ -57,36 +57,56 @@ const multiplyButton = document.querySelector(`.multiply`);
 const divideButton = document.querySelector(`.divide`);
 const equalsButton = document.querySelector(`.equals`);
 
-// alright so 
-// Init: secondNumber is empty string. First Number is zero. 
-//we'll have it be so that if a button is clicked, that number is added to the string secondNumber. If an operator is clicked, then that string is converted to a number firstNumber and that operator is stored as well. The display is cleared. secondNumber is now an empty string. When you select a number it'll add it to the string secondNumber. If equalsButton is clicked, secondNumber is converted to a number and added to firstNumber, and then firstNumber is displayed. If another operator is clicked, then  secondNumber is still converted to a number and added to firstNumber, but the screen is cleared and awaits the next input.
+
 one.addEventListener('click', () => {
     secondNumber = secondNumber.concat('1');
+    display.textContent = secondNumber;
 } );
 two.addEventListener('click', () => {
     secondNumber = secondNumber.concat('2');
+    display.textContent = secondNumber;
 } );
 three.addEventListener('click', () => {
     secondNumber = secondNumber.concat('3');
+    display.textContent = secondNumber;
 } );
 four.addEventListener('click', () => {
     secondNumber = secondNumber.concat('4');
+    display.textContent = secondNumber;
 } );
 five.addEventListener('click', () => {
     secondNumber = secondNumber.concat('5');
+    display.textContent = secondNumber;
 } );
 six.addEventListener('click', () => {
     secondNumber = secondNumber.concat('6');
+    display.textContent = secondNumber;
 } );
 seven.addEventListener('click', () => {
     secondNumber = secondNumber.concat('7');
+    display.textContent = secondNumber;
 } );
 eight.addEventListener('click', () => {
     secondNumber = secondNumber.concat('8');
+    display.textContent = secondNumber;
 } );
 nine.addEventListener('click', () => {
     secondNumber = secondNumber.concat('9');
+    display.textContent = secondNumber;
 } );
 zero.addEventListener('click', () => {
     secondNumber = secondNumber.concat('0');
+    display.textContent = secondNumber;
 } );
+clear.addEventListener('click', () => {
+    secondNumber = '';
+    firstNumber = 0;
+    operator = "Nothing selected yet";
+    display.textContent = secondNumber;
+} );
+
+// alright so 
+// Init: secondNumber is empty string. First Number is zero. 
+//we'll have it be so that if a button is clicked, that number is added to the string secondNumber. If an operator is clicked, then that string is converted to a number firstNumber and that operator is stored as well. The display is cleared. secondNumber is now an empty string. When you select a number it'll add it to the string secondNumber. If equalsButton is clicked, secondNumber is converted to a number and added to firstNumber, and then firstNumber is displayed. If another operator is clicked, then  secondNumber is still converted to a number and added to firstNumber, but the screen is cleared and awaits the next input.
+
+display.textContent = secondNumber;
